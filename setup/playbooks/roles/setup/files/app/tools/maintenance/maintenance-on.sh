431ba45dc85248722c0d677aae56330231e1e33d
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-08-02 15:11:16 +0200

    fix: disable tty in scripts

--- setup/playbooks/roles/setup/files/app/tools/maintenance/maintenance-on.sh
+++ setup/playbooks/roles/setup/files/app/tools/maintenance/maintenance-on.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 #Needs to be run as sudo
 
 echo "Mise en place de la page de maintenance..."
-docker exec -ti $(docker ps -q -f name={{product_name}}_reverse_proxy --latest) bash -c "mkdir -p /etc/nginx/html; touch /etc/nginx/html/maintenance.on" || true
+docker exec -i $(docker ps -q -f name={{product_name}}_reverse_proxy --latest) bash -c "mkdir -p /etc/nginx/html; touch /etc/nginx/html/maintenance.on" || true
