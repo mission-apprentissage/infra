c591a6938b3f5a6383c28c86285cd5cc165abca5
Antoine Bigard <bigard.antoine@gmail.com>
2023-08-02 18:00:08 +0200

    feat: move edit vault

--- setup/vault/edit-vault.sh
+++ setup/vault/edit-vault.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-readonly VAULT_FILE="${SCRIPT_DIR}/../../vault/vault.yml"
+readonly VAULT_FILE="${SCRIPT_DIR}/vault.yml"
 
 ansible-vault edit --vault-password-file="${SCRIPT_DIR}/get-vault-password-client.sh" "${VAULT_FILE}"
