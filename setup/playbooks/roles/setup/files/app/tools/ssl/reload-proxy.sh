ac71a1acb7316096e32d2dec1ceebb0f593c86c0
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-08-02 14:14:31 +0200

    fix: reload & renew certificate

--- setup/playbooks/roles/setup/files/app/tools/ssl/reload-proxy.sh
+++ setup/playbooks/roles/setup/files/app/tools/ssl/reload-proxy.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 for containerId in $(docker ps -f name={{product_name}}_reverse_proxy --quiet)
 do
-  docker exec -it $containerId nginx -s reload
+  docker exec -i $containerId nginx -s reload
 done
