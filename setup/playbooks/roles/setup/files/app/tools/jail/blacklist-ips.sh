9d2d15535d258096e06d8dde8db5003a2df57800
Antoine Bigard <bigard.antoine@gmail.com>
2023-10-12 16:28:49 +0200

    feat: add specific blacklist ips

--- setup/playbooks/roles/setup/files/app/tools/jail/blacklist-ips.sh
+++ setup/playbooks/roles/setup/files/app/tools/jail/blacklist-ips.sh
@@ -8,6 +8,11 @@ set -euo pipefail
 ipset -exist create ipsum hash:net
 ipset flush ipsum
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1); do ipset add ipsum $ip; done
+
+# specific blacklist
+ipset add ipsum 3.92.127.83
+ipset add ipsum 82.165.82.41
+
 iptables -D INPUT -m set --match-set ipsum src -j DROP 2>/dev/null || true
 iptables -I INPUT -m set --match-set ipsum src -j DROP
 
