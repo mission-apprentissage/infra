3895528e557ebf0c8791268b7ceaeb02f714d7f7
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-12-07 16:55:16 +0100

    feat: send fluentd logs in S3

--- .infra/files/app/tools/send-to-slack.sh
+++ .infra/files/app/tools/send-to-slack.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 
 readonly TEXT_MESSAGE=${1:?"Please provide a text message"}
 readonly SLACK_URL="{{ vault[product_name].SLACK_WEBHOOK_URL }}"
-readonly CHANNEL_NAME="{{ vault[product_name].SLACK_CHANNEL_NAME }}"
-readonly MNA_ENV=$(cat /env)
+readonly MNA_ENV="{{ product_name }}_{{ env_type }}"
 
 curl -s -o /dev/null -X POST --data-urlencode \
-  "payload={\"text\": \"[${MNA_ENV}] ${TEXT_MESSAGE}\", \"channel\": \"${CHANNEL_NAME}\" }" "${SLACK_URL}"
+  "payload={\"text\": \"[${MNA_ENV}] ${TEXT_MESSAGE}\" }" "${SLACK_URL}"
