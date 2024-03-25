2cfec82717c7ace556e6d5b55629d66f235ae455
Kevin Barnoin <k.barnoin@gmail.com>
2024-03-25 17:58:16 +0100

    feat: update script

--- .bin/scripts/backup-database.sh
+++ .bin/scripts/backup-database.sh
@@ -8,29 +8,24 @@ readonly VAULT_FILE="${ROOT_DIR}/.infra/vault/vault.yml"
 readonly BACKUP_LOCAL_DIR="/opt/app/mongodb/backups"
 readonly BACKUP_FILE="${BACKUP_LOCAL_DIR}/mongodb_$(date +%Y-%m-%d_%H-%M-%S).gpg"
 
-# Ansible vault password retrieval
-if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE:-}" ]]; then
-  ansible_extra_opts+=("--vault-password-file" "${SCRIPT_DIR}/get-vault-password-client.sh")
-else
-  echo "Récupération de la passphrase depuis l'environnement variable ANSIBLE_VAULT_PASSWORD_FILE"
-fi
+function backup() {
+  echo "Creating backup..."
+  mkdir -p "${BACKUP_LOCAL_DIR}"
+  docker run --rm mongo:6 mongodump --uri="{{ vault.${PRODUCT_NAME}.MONGODB_URI }}" -j=2 --gzip --archive \
+  | bash "${SCRIPT_DIR}/gpg/encrypt.sh" "$BACKUP_FILE"
+}
 
-# Get MONGODB_URI from Ansible Vault
-readonly MONGODB_URI=$(ansible-vault view "${ansible_extra_opts[@]}" "$VAULT_FILE" | yq ".vault.${PRODUCT_NAME}.MONGODB_URI")
+function uploadToS3(){
+  # upload gpg file to S3
+  echo "Uploading backup to S3..."
+}
 
-# Error handling for empty MONGODB_URI
-if [[ -z "$MONGODB_URI" ]]; then
-  echo "Error: MONGODB_URI not found in Ansible Vault." >&2
-  exit 1
-fi
+function deleteLocalFile(){
+  # keep 5 days of archive
+  echo "Removing old MongoDB backups..."
+  find "${BACKUP_LOCAL_DIR}" -mindepth 1 -maxdepth 1 -prune -ctime +5 -exec rm -r "{}" \;
+}
 
-# Run mongodump with captured URI and redirect output for encryption
-docker run --rm mongo:6 mongodump --uri="$MONGODB_URI" -j=2 --gzip --archive | source "${SCRIPT_DIR}/encrypt_script.sh" "$BACKUP_FILE"
-
-# Handle errors from the encryption script (exit code non-zero)
-if [[ $? -ne 0 ]]; then
-  echo "Error: Encryption failed." >&2
-  exit 1
-fi
-
-echo "MongoDB backup created and encrypted: $BACKUP_FILE"
+backup
+uploadToS3
+deleteLocalFile
