3d09a3f7226e94f910ca6d9798f30e30b8819317
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-11-08 14:49:44 +0100

    fix: apt locked workaround

---
+++ .infra/files/app/tools/workaround/apt-daily.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Check if apt is locked
+PID=$(lsof -t /var/lib/apt/lists/lock)
+
+# If apt is not locked, exit
+if [ -z "$PID" ]; then
+  exit 0
+fi
+
+# If apt is locked, check for how long
+TIME=$(ps -p $PID -o etimes=)
+
+# If apt is locked for more than 1 hour, kill the process
+if [ "$TIME" -gt 3600 ]; then
+  echo "apt process is stuck"
+  kill -SIGTERM $PID
+fi
