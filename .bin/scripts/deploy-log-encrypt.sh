eb7299a2b9ab3ecefe131bd0d42658fe25086454
mathieu <mathieu.borrell@beta.gouv.fr>
2025-07-24 23:29:50 +0200

    fix: utilisation de la commande shred (#137)
    * fix: suppression de la commande qui tue l'agent GnuPG
    
    * fix: remplacement de la commande rm par shred
    
    * feat: ajout d'un contrôle sur les commandes requises
    
    * fix: correction des résultats renvoyés par yq

--- .bin/scripts/deploy-log-encrypt.sh
+++ .bin/scripts/deploy-log-encrypt.sh
@@ -12,11 +12,13 @@ readonly PASSPHRASE="$ROOT_DIR/.bin/DEPLOY_GPG_PASSPHRASE.txt"
 readonly VAULT_FILE="${ROOT_DIR}/products/infra/vault/vault.yml"
 
 delete_cleartext() {
-  rm -f "$PASSPHRASE"
+  if [ -f "$PASSPHRASE" ]; then
+    shred -f -n 10 -u "$PASSPHRASE"
+  fi
 }
 trap delete_cleartext EXIT
 
-ansible-vault view "${ansible_extra_opts[@]}" "$VAULT_FILE" | yq '.vault.DEPLOY_GPG_PASSPHRASE' > "$PASSPHRASE"
+ansible-vault view "${ansible_extra_opts[@]}" "$VAULT_FILE" | yq -r '.vault.DEPLOY_GPG_PASSPHRASE' > "$PASSPHRASE"
 
 # Make sur the file exists
 touch /tmp/deploy.log
