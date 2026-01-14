3804fb278da5d921f29a23ac0957101b78d4d744
mathieu <mathieu.borrell@beta.gouv.fr>
2026-01-14 20:47:07 +0100

    feat: suppression du cache Nginx lors de son rechargement

--- reverse_proxy/files/reload.sh
+++ reverse_proxy/files/reload.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 
 # Update templates files conversion
+
 /docker-entrypoint.d/20-envsubst-on-templates.sh
+
 # Validate file configuration
+
 nginx -t
+
+# Remove cache files
+
+rm -r /tmp/nginx_cache
+
 # Trigger nginx reload
+
 nginx -s reload
